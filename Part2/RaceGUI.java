0bf267b0b39f8fc2018a3740f07a437cad4a9496
Julius <phanduc1809@gmail.com>
2025-04-29 01:44:52 +0100

    fix library list in RaceGUI

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -1,7 +1,8 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RaceGUI extends JFrame {
     private TrackPanel trackPanel;
