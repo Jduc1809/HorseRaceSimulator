197e1869bc27cf1dde83a5bb2ea68b80de2dc851
Julius <phanduc1809@gmail.com>
2025-04-29 01:41:37 +0100

    add TrackPanel code

---
+++ Part2/TrackPanel.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.Random;
+
+public class TrackPanel extends JPanel {
+    private List<HorseGUI> horses;
+    private int raceLength = 800; // pixels
+    private Random rand = new Random();
+
+    public TrackPanel(List<HorseGUI> horses) {
+        this.horses = horses;
+        setPreferredSize(new Dimension(raceLength + 100, horses.size() * 100));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draw lanes
+        for (int i = 0; i < horses.size(); i++) {
+            g.setColor(Color.GRAY);
+            g.fillRect(0, i * 100 + 40, raceLength, 5); // lane line
+        }
+
+        // Draw horses
+        for (int i = 0; i < horses.size(); i++) {
+            HorseGUI horse = horses.get(i);
+
+            g.setColor(horse.getColor());
+            int x = horse.getDistanceTravelled();
+            int y = i * 100 + 10;
+
+            g.drawString(horse.getSymbol() + " " + horse.getName(), x, y);
+        }
+    }
+
+    public void updateRace() {
+        for (HorseGUI horse : horses) {
+            if (!horse.hasFallen()) {
+                if (rand.nextDouble() < horse.getConfidence()) {
+                    horse.moveForward();
+                }
+                if (rand.nextDouble() < (0.1 * (1 - horse.getConfidence()))) {
+                    horse.fall();
+                }
+            }
+        }
+        repaint();
+    }
+
+    public HorseGUI checkWinner() {
+        for (HorseGUI horse : horses) {
+            if (horse.getDistanceTravelled() >= raceLength) {
+                return horse;
+            }
+        }
+        return null;
+    }
+}
